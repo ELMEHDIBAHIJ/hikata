113a2d32adc6e90453de5b26ca19e01641d2471c
ELMEHDIBAHIJ <bahijstudies@gmail.com>
2022-11-14 14:25:04 +0100

    newupdate

--- funct.js
+++ funct.js
@@ -1,238 +1,6 @@
-// const header = document.querySelector("header");
-
-// window.addEventListener ("scroll", function() {
-// 	header.classList.toggle ("sticky", window.scrollY > 0);
-// });
-
-// let menu = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
-
-// menu.onclick = () => {
-// 	menu.classList.toggle('bx-x');
-// 	navbar.classList.toggle('open');
-// };
-
-// window.onscroll = () => {
-// 	menu.classList.remove('bx-x');
-// 	navbar.classList.remove('open');
-// };
-// // adding function
-// // var P = 0; 
-// // function add(name,price) {
-// //     var firstDivContent = document.getElementById(name);
-// //     var outDivContent = document.getElementById(price);
-// //     var product = document.getElementById('e');
-// //     var total = document.getElementById('htotal');
-
-// //     let R = document.createElement("tr");
-// //     let A = document.createElement("td");
-// //     let B = document.createElement("td");
-   
-    
-// //     product.append(R);
-// //     A.innerHTML = firstDivContent.innerHTML;
-// //     B.innerHTML = outDivContent.innerHTML + "$";
-    
-    
-// //     var Prix =Number(outDivContent.innerHTML);
-    
-// //     P = P + Prix;
-    
-// //     R.append(A,B);
-// //     total.innerHTML=P+"$";
-// //     }
-// const header = document.querySelector("header");
-
-// window.addEventListener ("scroll", function() {
-//     header.classList.toggle ("sticky", window.scrollY > 0);
-// });
-
-// let menu = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
-
-// menu.onclick = () => {
-//     menu.classList.toggle('bx-x');
-//     navbar.classList.toggle('open');
-// };
-
-// window.onscroll = () => {
-//     menu.classList.remove('bx-x');
-//     navbar.classList.remove('open');
-// };
-//         let togg1 = document.getElementById("togg1");
-//     let togg2 = document.getElementById("togg2");
-//     let d1 = document.getElementById("d1");
-//     let d2 = document.getElementById("d2");
-//     togg1.addEventListener("click", () => {
-//       if(getComputedStyle(d1).display != "none"){
-//         d1.style.display = "none";
-//       } else {
-//         d1.style.display = "block";
-//       }
-//     })
-
-//     function togg(){
-//       if(getComputedStyle(d2).display != "none"){
-//         d2.style.display = "none";
-//       } else {
-//         d2.style.display = "block";
-//       }
-//     };
-//     togg2.onclick = togg;
-// var P = 0; 
-//         function add(name,price) {
-//             var firstDivContent = document.getElementById(name);
-//             var outDivContent = document.getElementById(price);
-//             var product = document.getElementById('e');
-//             var total = document.getElementById('htotal');
-
-//             let R = document.createElement("tr");
-//             let A = document.createElement("td");
-//             let B = document.createElement("td");
 
 
-//             product.append(R);
-//             A.innerHTML = firstDivContent.innerHTML;
-//             B.innerHTML = outDivContent.innerHTML + "$";
 
-
-//             var Prix =Number(outDivContent.innerHTML);
-
-//             P = P + Prix;
-
-//             R.append(A,B);
-//             total.innerHTML=P+"$";
-//             }
-// // end of adding function
-    
-//     function filter(item){
-//         var sushi1=document.getElementById('c3');
-//         var sushi2=document.getElementById('c4');
-//         var sushi3=document.getElementById('c5');
-//         var sushi4=document.getElementById('c6');
-//         var soup1=document.getElementById('c1');
-//         var soup2=document.getElementById('c2');
-//         var salade1=document.getElementById('c7');
-//         var salade2=document.getElementById('c8');
-//         if(item=='all'){
-//             sushi1.style.display = "block"
-//             sushi2.style.display = "block"
-//             sushi3.style.display = "block"
-//             sushi4.style.display = "block"
-//             soup1.style.display = "block"
-//             soup2.style.display = "block"
-//             salade1.style.display = "block"
-//             salade2.style.display = "block"
-            
-    
-            
-//         }
-//         if(item=='sushi'){
-//             sushi1.style.display = "block"
-//             sushi2.style.display = "block"
-//             sushi3.style.display = "block"
-//             sushi4.style.display = "block"
-//             soup1.style.display = "none"
-//             soup2.style.display = "none"
-//             salade1.style.display = "none"
-//             salade2.style.display = "none"
-    
-//         }
-//         if(item=='salade'){
-//             sushi1.style.display = "none"
-//             sushi2.style.display = "none"
-//             sushi3.style.display = "none"
-//             sushi4.style.display = "none"
-//             soup1.style.display = "none"
-//             soup2.style.display = "none"
-//             salade1.style.display = "block"
-//             salade2.style.display = "block"
-    
-//         }
-//         if(item=='soup'){
-//             sushi1.style.display = "none"
-//             sushi2.style.display = "none"
-//             sushi3.style.display = "none"
-//             sushi4.style.display = "none"
-//             soup1.style.display = "block"
-//             soup2.style.display = "block"
-//             salade1.style.display = "none"
-//             salade2.style.display = "none"
-    
-//         }
-//     }
-//     var a=document.getElementById('menu').value;
-//     console.log(a)
-//     function select(a){
-    
-   
-
-// }
-// const header = document.querySelector("header");
-
-// window.addEventListener ("scroll", function() {
-//     header.classList.toggle ("sticky", window.scrollY > 0);
-// });
-
-// let menu = document.querySelector('#menu-icon');
-// let navbar = document.querySelector('.navbar');
-
-// menu.onclick = () => {
-//     menu.classList.toggle('bx-x');
-//     navbar.classList.toggle('open');
-// };
-
-// window.onscroll = () => {
-//     menu.classList.remove('bx-x');
-//     navbar.classList.remove('open');
-// };
-//         let togg1 = document.getElementById("togg1");
-//     let togg2 = document.getElementById("togg2");
-//     let d1 = document.getElementById("d1");
-//     let d2 = document.getElementById("d2");
-//     togg1.addEventListener("click", () => {
-//       if(getComputedStyle(d1).display != "none"){
-//         d1.style.display = "none";
-//       } else {
-//         d1.style.display = "block";
-//       }
-//     })
-
-//     function togg(){
-//       if(getComputedStyle(d2).display != "none"){
-//         d2.style.display = "none";
-//       } else {
-//         d2.style.display = "block";
-//       }
-//     };
-//     togg2.onclick = togg;
-
-
-//         var P = 0; 
-//         function add(name,price) {
-//             var firstDivContent = document.getElementById(name);
-//             var outDivContent = document.getElementById(price);
-//             var product = document.getElementById('e');
-//             var total = document.getElementById('htotal');
-
-//             let R = document.createElement("tr");
-//             let A = document.createElement("td");
-//             let B = document.createElement("td");
-//             console.log(R);
-
-
-//             product.append(R);
-//             A.innerHTML = firstDivContent.innerHTML;
-//             B.innerHTML = outDivContent.innerHTML + "$";
-
-
-//             var Prix =Number(outDivContent.innerHTML);
-
-//             P = P + Prix;
-
-//             R.append(A,B);
-//             total.innerHTML=P+"$";
-//             }
 
 
 
@@ -241,35 +9,48 @@
 
 
        function filter(value){
-                         var value=document.getElementById('fltr').value;
-                         var sushi=document.getElementById('sushi');
-                         var maguro=document.getElementById('maguro');
-                         //var ahi=document.getElementsByClassName('ahi');
-                        
-                         if(value=='0'){
-                            sushi.style.display = "block"
-                            maguro.style.display = "block";
-                           
-                       }
-                        if(value=='1'){
-                            sushi.style.display = "block";
-                            maguro.style.display = "none";
-                          
-                        }
-                        if(value=='2'){
-                            sushi.style.display = "none";
-                            maguro.style.display = "block";
-                            
-                        }
-                        if(value=='3'){
-                            sushi.style.display = "none";
-                            maguro.style.display = "none";
-                            //ahi.style.display = "block";
-                        }
-                      
-                console.log(value);
-              
-            }
+        var value=document.getElementById('fltr').value;
+        var sushi=document.getElementById('sushi');
+        var maguro=document.getElementById('maguro');
+        //var ahi=document.getElementsByClassName('ahi');
+       
+        if(value=='0'){
+           sushi.style.display = "block"
+           maguro.style.display = "block";
+          
+      }
+       if(value=='1'){
+           sushi.style.display = "block";
+           maguro.style.display = "none";
+         
+       }
+       if(value=='2'){
+           sushi.style.display = "none";
+           maguro.style.display = "block";
+           
+       }
+       if(value=='3'){
+           sushi.style.display = "none";
+           maguro.style.display = "none";
+           //ahi.style.display = "block";
+       }
+     
+console.log(value);
+
+}
+
+
+
+// navbar toggler
+
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+console.log(menu);
+
+menu.addEventListener('click', function() {
+navbar.classList.toggle('hide')
+})
 
 
 
@@ -280,40 +61,56 @@
 
 
 
+// pannel toggler
+
+let purchase = document.querySelector('.purchase');
+let pannel = document.querySelector('.pannel');
+
+
+purchase.addEventListener('click', function() {
+pannel.classList.toggle('show-pannel')
+})
 
 
 
 
 
 
+// menu card
+
+let addButtons = document.querySelectorAll('.add-button')
+
+addButtons.forEach(function(button) {
+button.addEventListener('click', function() {
+let price =  button.parentElement.querySelector('p').textContent
+let name = button.parentElement.parentElement.querySelector('p').textContent
+
+let total = document.querySelector('#total')
+
+let totalValue = Number.parseInt(total.textContent)
+totalValue += Number.parseInt(price) 
+total.textContent = totalValue
+
+
+let pannel = document.querySelector('.pannel')
+
+
+let nameElement = document.createElement('p')
+let priceElement = document.createElement('p')
+let div = document.createElement('div')
+div.style.display = 'flex'
+div.style.justifyContent = 'space-between'
+
+nameElement.textContent = name
+priceElement.textContent = price
+
+div.append(nameElement, priceElement)
+pannel.append(div)
+
+})
+})
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-            // console.log("dddd");
-            // // console.log(document.getElementsByClassName('ahi'));
-            
-            // let a = document.getElementsByClassName('ahi')
-            // console.log(a)
-            // let b = []
-            // for (let index = 0; index < a.length; index++) {
-            //     console.log(a[index]);
-            //     a[index].style.display = "none"
-            //     console.log(a[index].className);
-            //     b.push(a[index].className)
-            // }
-            // console.log(b);
-            // a.forEach(element => {
-            //     element.style.display = "none"
-            // });
